2995a3974f561d3c7c8120d74f114dd01ce44472
Yusuf Naheem <Yusuf.Naheem@vercoglobal.com>
2023-10-09 16:25:31 +0100

    Add project files.

---
+++ SSO.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SSO.Client.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
